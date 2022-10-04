d3f5249b493a68517abc2f3e356ae959f256195b
MichaelK <havinmichael@gmail.com>
2022-10-04 20:15:54 +0300

    fix!: Исправлены ошибки в AuthenticationView + прочие исправления ошибок

--- FriendsFeed/AuthenticationModule/AuthenticationView.swift
+++ FriendsFeed/AuthenticationModule/AuthenticationView.swift
@@ -16,18 +16,6 @@ final class AuthenticationView: UIView {
         view.contentMode = .scaleAspectFit
         return view
     }()
-    private lazy var buttonsStack: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [
-            registerButton,
-            logInButton
-        ])
-        stackView.alignment = .fill
-        stackView.spacing = 30
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-        
-        return stackView
-    }()
     private(set) lazy var registerButton: UIButton = {
         let view = UIButton()
         view.backgroundColor = .label
@@ -36,18 +24,11 @@ final class AuthenticationView: UIView {
         view.layer.shadowOffset = CGSize(width: 0, height: 2)
         view.layer.shadowOpacity = 1
         
-        view.setTitle("ЗАРЕГИСТРИРОВАТЬСЯ", for: .normal)
+        view.setTitle("ВОЙТИ", for: .normal)
         view.titleLabel?.font = .preferredFont(forTextStyle: .title1)
         view.titleLabel?.adjustsFontSizeToFitWidth = true
         view.titleLabel?.minimumScaleFactor = 0.2
         view.setTitleColor(UIColor.systemBackground, for: .normal)
-        return view
-    }()
-    private(set) lazy var logInButton: UIButton = {
-        let view = UIButton()
-        view.setTitle("Уже есть аккаунт", for: .normal)
-        view.titleLabel?.font = .preferredFont(forTextStyle: .title3)
-        view.setTitleColor(UIColor.label, for: . normal)
         return view
     }()
     
@@ -64,9 +45,9 @@ final class AuthenticationView: UIView {
     }
     
     override func updateConstraints() {
-        buttonsStack.snp.updateConstraints { make in
-            make.bottom.equalTo(safeAreaLayoutGuide).inset(frame.height/4)
+        registerButton.snp.updateConstraints { make in
             make.leading.trailing.equalTo(layoutMarginsGuide).inset(frame.width/6)
+            make.top.equalTo(safeAreaLayoutGuide.snp.centerY).offset(frame.height/4)
         }
         registerButton.layer.cornerRadius = frame.height / 100
         
@@ -81,11 +62,11 @@ final class AuthenticationView: UIView {
             make.bottom.equalTo(safeAreaLayoutGuide.snp.centerY)
         }
         
-        addSubview(buttonsStack)
-        buttonsStack.snp.makeConstraints { make in
+        addSubview(registerButton)
+        registerButton.snp.makeConstraints { make in
             make.leading.trailing.equalTo(layoutMarginsGuide).inset(frame.width/6)
-            make.top.equalTo(safeAreaLayoutGuide.snp.centerY).offset(40)
-            make.bottom.equalTo(safeAreaLayoutGuide).inset(frame.height/4)
+            make.top.equalTo(safeAreaLayoutGuide.snp.centerY).offset(frame.height/4)
+            make.height.equalTo(50)
         }
     }
 }
--- FriendsFeed/LogInModule/View/LogInViewController.swift
+++ FriendsFeed/LogInModule/View/LogInViewController.swift
@@ -47,6 +47,7 @@ class LogInViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        navigationController?.isNavigationBarHidden = false
         setNotifications()
     }
     
